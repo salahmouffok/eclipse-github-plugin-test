fc3203b97afe48cd9a439dc28c1cf20f9107227f
Salah <salaheddine.mouffok@hotmail.com>
2021-06-27 17:56:24 +0200

    Add getter and setter

--- src/com/salah/User.java
+++ src/com/salah/User.java
@@ -3,5 +3,13 @@ package com.salah;
 public class User {
     
     private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
     
 }
